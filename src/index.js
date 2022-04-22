3b21feb38e28d822bd848a09d5e8ac48a31e661d
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-04-22 11:24:59 -0400

    added features section

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+import Features from "./Components/Features/Features";
 import HeroSection from "./Components/HeroSection/HeroSection"
 import NavBar from "./Components/Navigation/Nav"
 
 const App = () => {
     return (
-        <div>
 
+        <div>
 
             <NavBar />
             <HeroSection />
+            <Features />
 
         </div>
 
